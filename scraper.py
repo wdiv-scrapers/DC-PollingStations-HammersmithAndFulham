cca90a2de30706da6add350f794cef85b8309f8c
Will Roper <GeoWill@users.noreply.github.com>
2022-04-07 11:49:20 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 #search_url = "https://opendata.arcgis.com/api/v2/datasets?filter[catalogs]=data-lbhf.opendata.arcgis.com&include=organizations%2Cgroups&page[number]=1&page[size]=10&q=polling&sort=-updatedAt"
 stations_url = "https://data-lbhf.opendata.arcgis.com/datasets/c15c9a3aca054d4cb7c6b3f84779d64d_0.geojson"
 districts_url = "https://data-lbhf.opendata.arcgis.com/datasets/c15c9a3aca054d4cb7c6b3f84779d64d_11.geojson"
-council_id = "E09000013"
+council_id = "HMF"
 
 
 #search_scraper = HashOnlyScraper(search_url, council_id, 'datasets', 'json')
